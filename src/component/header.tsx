c14518e608ec59571d5c0ac70d5bceebc55d19a4
charlie-whight <charles.whight@rvu.co.uk>
2022-02-24 21:36:17 +0000

    sizing

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,6 +1,6 @@
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import {faChartBar, faInfo} from "@fortawesome/free-solid-svg-icons";
 
 interface HeaderProps {
@@ -15,10 +15,14 @@ const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
 
     return (
         <div className={`header mb-3`}>
-            <div className={"w-90 d-flex justify-content-between align-items-end"}>
-                <FontAwesomeIcon className={"header-links "} icon={faChartBar}/>
+            <div className={"w-90 d-flex justify-content-between align-items-center"}>
+                <h2 className={"header-content header-links mb-0"} onClick={() => showScores()}>
+                    <FontAwesomeIcon icon={faChartBar} />
+                </h2>
                 <h1 className={"header-content mb-0 page-title"}>Numble</h1>
-                <FontAwesomeIcon className={"header-links "} icon={faInfo}/>
+                <h2 className={"header-content header-links mb-0"} onClick={() => showRules()}>
+                    <FontAwesomeIcon className={"header-links "} icon={faInfo} />
+                </h2>
             </div>
         </div>
     )
